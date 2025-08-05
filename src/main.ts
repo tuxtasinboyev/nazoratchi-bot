1e1d948e69f79235a74ca3fb65ddf0081132f29f
Omadbek Tuxtasinboyev <omadbek1223erubdnx@gmail.com>
2025-08-05 20:31:58 +0500

    nazoratchi bot yozildi tugrilandi

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,28 @@
 import { NestFactory } from '@nestjs/core';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.RMQ,
-      options: {
-        urls: ['amqps://fhrzlrdw:hDfPFXgmeA3RbotR3urp0T3Dp2x8u7NY@kebnekaise.lmq.cloudamqp.com/fhrzlrdw'],
-        queue: 'observer_bot_queue',
-          queueOptions: {
-            durable: false,
-          },
+  const app = await NestFactory.create(AppModule); 
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [
+        'amqps://fhrzlrdw:hDfPFXgmeA3RbotR3urp0T3Dp2x8u7NY@kebnekaise.lmq.cloudamqp.com/fhrzlrdw',
+      ],
+      queue: 'observer_bot_queue',
+      queueOptions: {
+        durable: false,
       },
     },
-  );
+  });
 
-  await app.listen();
-  console.log('🟢 Observer Bot microservice is listening...');
+  await app.startAllMicroservices();
+  console.log('🟢 Observer Bot Microservice is listening via RabbitMQ');
+
+  await app.listen(process.env.PORT || 3000);
+  console.log(`🌐 Fake HTTP server ishlayapti portda: ${process.env.PORT || 3000}`);
 }
+
 bootstrap();
