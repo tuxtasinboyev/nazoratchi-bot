0efa9b5e25c787312df901e99b3b630e6a8c20a2
Omadbek Tuxtasinboyev <omadbek1223erubdnx@gmail.com>
2025-08-06 02:30:50 +0500

    bot tugirlandivm

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,28 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+  import { NestFactory } from '@nestjs/core';
+  import { AppModule } from './app.module';
+  import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule); 
+  async function bootstrap() {
+    const app = await NestFactory.create(AppModule); 
 
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.RMQ,
-    options: {
-      urls: [
-        'amqps://fhrzlrdw:hDfPFXgmeA3RbotR3urp0T3Dp2x8u7NY@kebnekaise.lmq.cloudamqp.com/fhrzlrdw',
-      ],
-      queue: 'observer_bot_queue',
-      queueOptions: {
-        durable: false,
+    app.connectMicroservice<MicroserviceOptions>({
+      transport: Transport.RMQ,
+      options: {
+        urls: [
+          'amqps://fhrzlrdw:hDfPFXgmeA3RbotR3urp0T3Dp2x8u7NY@kebnekaise.lmq.cloudamqp.com/fhrzlrdw',
+        ],
+        queue: 'observer_bot_queue',
+        queueOptions: {
+          durable: false,
+        },
       },
-    },
-  });
+    });
 
-  await app.startAllMicroservices();
-  console.log('🟢 Observer Bot Microservice is listening via RabbitMQ');
+    await app.startAllMicroservices();
+    console.log('🟢 Observer Bot Microservice is listening via RabbitMQ');
 
-  await app.listen(process.env.PORT || 3000);
-  console.log(`🌐 Fake HTTP server ishlayapti portda: ${process.env.PORT || 3000}`);
-}
+    // await app.listen(process.env.PORT || 3000);
+    // console.log(`🌐 Fake HTTP server ishlayapti portda: ${process.env.PORT || 3000}`);
+  }
 
-bootstrap();
+  bootstrap();
